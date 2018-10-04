ea0683a29e1e12b0dfa63726e62cf11b46de6fe9
Pierce Bartine <piercebartine@gmail.com>
2018-10-03 20:13:53 -0500

    Basic plugin funcationality

---
+++ terraform.plugin.zsh
@@ -0,0 +1,20 @@
+#!/usr/bin/env zsh
+
+alias tf=terraform
+alias tfw="terraform workspace"
+
+tfv(){
+  terraform validate -var-file=vars/$(terraform workspace show).tfvars
+}
+
+tfp(){
+  terraform plan -var-file=vars/$(terraform workspace show).tfvars
+}
+
+tfa(){
+  terraform apply -var-file=vars/$(terraform workspace show).tfvars
+}
+
+tfc(){
+  terraform console -var-file=vars/$(terraform workspace show).tfvars
+}
